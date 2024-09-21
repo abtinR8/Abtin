374c801bc5434f06bd974162a972234525393e09
Abtin.r <abtinrezaie@gmail.com>
2024-09-21 12:23:33 +0330

    fixed navbar

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const navToggle = document.getElementById("nav-toggle");
 const closeIcon = document.getElementById("x-icon");
 const grayDispaly = document.getElementById("gray-dispaly");
 const welcomeText = document.getElementById("welcome-text");
+const navbar = document.querySelector(".nav");
+const navContainer = document.querySelector(".nav-container");
+const welcomeTextEllips = document.querySelector(".nav-menu-ellips")
 
 function showMenu() {
   navMenu.classList.toggle("showMenu");
@@ -12,9 +15,23 @@ function showMenu() {
 function changeWelcomeText() {
   if (window.innerWidth >= 992) {
     welcomeText.innerText = "Welcome to my page";
-  }else{
-    const br = document.createElement("br")
-    welcomeText.innerText = `Welcome to my page \n « Abtin »`
+  } else {
+    const br = document.createElement("br");
+    welcomeText.innerText = `Welcome to my page \n « Abtin »`;
+  }
+}
+
+function setNavbar() {
+  let navScroll = window.scrollY;
+  if (navScroll >= 90) {
+    navbar.classList.add("nav-fix");
+    navContainer.classList.add("nav-container-fix");
+    welcomeTextEllips.style.padding = "0.5rem 1rem"
+  } else {
+    navbar.classList.remove("nav-fix");
+    navContainer.classList.remove("nav-container-fix");
+    welcomeTextEllips.style.padding = "1rem"
+
   }
 }
 
@@ -22,3 +39,4 @@ navToggle.addEventListener("click", showMenu);
 closeIcon.addEventListener("click", showMenu);
 window.addEventListener("DOMContentLoaded", changeWelcomeText);
 window.addEventListener("resize", changeWelcomeText);
+window.addEventListener("scroll", setNavbar);
