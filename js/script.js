32630beeabfb2015dd31220515161c38654f106b
Abtin.r <abtinrezaie@gmail.com>
2024-09-24 20:03:27 +0330

    some edit

--- js/script.js
+++ js/script.js
@@ -74,7 +74,7 @@ function moveShapes(e) {
     }%), 150%)`;
   } else {
     shape1.style.transform = `translate(calc(-200% + ${
-      mousePosition * -5
+      mousePosition * -8
     }%), -300%)`;
     shape2.style.transform = `translate(calc(400% + ${
       mousePosition * -4
@@ -90,27 +90,27 @@ window.addEventListener("resize", initialLoading);
 window.addEventListener("scroll", setNavbar);
 hero.addEventListener("mousemove", moveShapes);
 
-// // Function to check if an element is in the viewport
-// function isElementInViewport(element) {
-//   const rect = element.getBoundingClientRect();
-//   return (
-//    rect.top >= 0 &&
-//    rect.left >= 0 &&
-//    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-//    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//   );
-//  }
-//  // Function to lazily load content
-//  function lazyLoadContent() {
-//   const lazyContentElements = document.querySelectorAll(".lazy-content");
-//   lazyContentElements.forEach((element) => {
-//    if (isElementInViewport(element)) {
-//     // Add your logic to load the content for the element here
-//     element.classList.add("loaded");
-//    }
-//   });
-//  }
-//  // Attach the lazyLoadContent function to the scroll event
-//  window.addEventListener("scroll", lazyLoadContent);
-//  // Call the function initially to load the visible content on page load
-//  lazyLoadContent();
+// Function to check if an element is in the viewport
+function isElementInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+   rect.top >= 0 &&
+   rect.left >= 0 &&
+   rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+   rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+ }
+ // Function to lazily load content
+ function lazyLoadContent() {
+  const lazyContentElements = document.querySelectorAll(".lazy-content");
+  lazyContentElements.forEach((element) => {
+   if (isElementInViewport(element)) {
+    // Add your logic to load the content for the element here
+    element.classList.add("loaded");
+   }
+  });
+ }
+//  Attach the lazyLoadContent function to the scroll event
+ window.addEventListener("scroll", lazyLoadContent);
+//  Call the function initially to load the visible content on page load
+ lazyLoadContent();
